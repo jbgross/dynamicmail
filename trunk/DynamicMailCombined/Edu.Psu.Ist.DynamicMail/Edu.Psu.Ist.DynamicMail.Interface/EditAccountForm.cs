88fa0cc0623549056b5ce0445808950568d841a6
joshuagrossist <joshuagrossist@b3e508ca-803a-0410-8912-73af84f046af>
2008-03-09 01:55:28 +0000

    somehow all the files got deleted - who knows?

---
+++ trunk/DynamicMailCombined/Edu.Psu.Ist.DynamicMail/Edu.Psu.Ist.DynamicMail.Interface/EditAccountForm.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Edu.Psu.Ist.DynamicMail.Interface
+{
+    /// <summary>
+    /// Class to add an individual account to a network
+    /// </summary>
+    public partial class EditAccountForm : Form
+    {
+        Finishable finishable;
+        
+        private String accountName = "";
+
+        /// <summary>
+        /// The name for the Account
+        /// </summary>
+        public String AccountName
+        {
+            get 
+            {
+                if (this.accountName == null)
+                {
+                    this.accountName = this.NameBox.Text;
+                }
+                return this.accountName; 
+            }
+            private set 
+            {
+                this.accountName = value; 
+                this.NameBox.Text = value; 
+            }
+        }
+
+        private String accountAddress = "";
+
+        /// <summary>
+        /// The address for the account
+        /// </summary>
+        public String AccountAddress
+        {
+            get
+            {
+                if (this.accountAddress == null)
+                {
+                    this.accountAddress = this.AddressBox.Text;
+                }
+                return this.accountAddress;
+            }
+            private set
+            {
+                this.accountAddress = value;
+                this.AddressBox.Text = value;
+            }
+        }
+
+        /// <summary>
+        /// Create a new instance and make visible
+        /// </summary>
+        /// <param name="finish"></param>
+        public EditAccountForm(Finishable finish)
+        {
+            InitializeComponent();
+            this.finishable = finish;
+            this.Show();
+            this.NameBox.Focus();
+        }
+
+        /// <summary>
+        /// Create a new instance and make visible
+        /// </summary>
+        /// <param name="finish"></param>
+        public EditAccountForm(Finishable finish, String name, String address)
+        {
+            InitializeComponent();
+            this.finishable = finish;
+            AccountName = name;
+            AccountAddress = address;
+            this.Show();
+            this.NameBox.Focus();
+        }
+
+        /// <summary>
+        /// Ignore this entire social network
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            this.finishable.Cancel();
+        }
+
+        /// <summary>
+        /// Add a new Account
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Add_Click(object sender, EventArgs e)
+        {
+            AccountName = NameBox.Text;
+            AccountAddress = AddressBox.Text;
+            this.Close();
+            this.finishable.Finish();
+        }
+
+    }
+}
---
+++ trunk/DynamicMailCombined/Edu.Psu.Ist.DynamicMail/Edu.Psu.Ist.Keystone.Vector/VectorException.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Edu.Psu.Ist.Keystone.Dimensions
+{
+    class VectorException : Exception
+    {
+        public VectorException(String msg)
+            : base(msg)
+        {
+        }
+    }
+}
